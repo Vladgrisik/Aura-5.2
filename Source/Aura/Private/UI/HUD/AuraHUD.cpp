f27b622e044dfbea5e5822c4abd65029d41c5fcf
vladgrisik <vladgrisik@gmail.com>
2024-04-20 20:53:14 +0300

    CreateHUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,15 @@
+// copyright Druid mechanics
+
+
+#include "UI/HUD/AuraHUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* UserWidget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	UserWidget->AddToViewport();
+}
